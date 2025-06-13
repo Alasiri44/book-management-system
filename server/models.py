7b472339aa12a3e88730b10622ad56e645f2e001
alasiri <austinalasiri44@gmail.com>
2025-06-13 14:52:34 +0300

    initial commit

---
+++ server/models.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
+
+# Association table to store many to many relationship between books and reviews
+book_reviews = db.Table(
+    'books_reviews',
+    metadata,
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), primary_key=True),
+    db.Column('review_id', db.Integer, db.ForeignKey('reviews.id'), primary_key=True)
+)
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    publication_year = db.Column(db.Integer)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    
+    # Relationship showing which author has written a book
+    author = db.relationship('Author',secondary=book_reviews, back_populates='books')
+    
+    reviews = db.relationship('Review', secondary=book_reviews, back_populates='books')
+    
+    def __repr__(self):
+        return f'<Book {self.id}, {self.title}, {self.publication_year}>'
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    
+    # Relationship referencing the author to books
+    books = db.relationship('Book', back_populates='author')
+    
+    def __repr__(self):
+        return f'<Author {self.id}, {self.name}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    links = db.Column(db.String)
+    
+    books = db.relationship('Book', secondary=book_reviews, back_populates='reviews')
+    
+    def __repr__(self):
+        return f'<Review {self.id}, {self.rating}, {self.comment}>'
